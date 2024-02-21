6795934cc7c991ad6ebe2f88dd49b240432e4369
letrinh1504 <nguyentrinh154@gmail.com>
2024-02-21 16:33:36 +0700

    Fist mess

---
+++ client/src/components/shop/productInCategary/index.js
@@ -0,0 +1,41 @@
+import React, { Fragment, createContext, useReducer } from "react";
+import Layout from "../layout";
+import Slider from "./Slider";
+import ProductCategory from "./ProductCategory";
+import { homeState, homeReducer } from "./HomeContext";
+import SingleProduct from "./SingleProduct";
+// import FollowONIG from "./FollowMe";
+
+export const HomeContext = createContext();
+
+const HomeComponent = () => {
+  return (
+    <Fragment>
+      <Slider />
+      {/* Category, Search & Filter Section */}
+      <section className="md:mx-8 md:my-6 bg-gray-100 border-2 pt-3 pl-2 pr-2 pb-3">
+        <ProductCategory />
+      </section>
+      {/* Product Section */}
+      {/* <section className="m-4 md:mx-8 md:my-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <SingleProduct />
+      </section> */}
+      {/* <section className="m-4 md:mx-8 md:my-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <FollowONIG />
+      </section> */}
+    </Fragment>
+  );
+};
+
+const Product = (props) => {
+  const [data, dispatch] = useReducer(homeReducer, homeState);
+  return (
+    <Fragment>
+      <HomeContext.Provider value={{ data, dispatch }}>
+        <Layout children={<HomeComponent />} />
+      </HomeContext.Provider>
+    </Fragment>
+  );
+};
+
+export default Product;
